287e5e84be6a9e537efc11738221d8d46aa03521
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 12:38:00 -0800

    configure settings

--- repository_similarity/urls.py
+++ repository_similarity/urls.py
@@ -21,4 +21,4 @@ from django.urls import path, include
 urlpatterns = [
         path("admin/", admin.site.urls),
         path("", include("Users.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
