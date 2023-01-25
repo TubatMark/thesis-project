7f29e539b863316e62bc50a899832b31dcb934eb
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 10:19:01 -0800

    delete staticfiles

--- repository_similarity/settings.py
+++ repository_similarity/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Users/static")]
 
