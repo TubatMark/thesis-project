e81c4dbc59e64ad2556d2b831a0b3d4f35bd4068
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 12:09:36 -0800

    debug to true

--- repository_similarity/settings.py
+++ repository_similarity/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9cd+5@f7gxnv5&b7(^s&n_3@n-@+u%r5^sy@7q4_x0t+72x#&%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
