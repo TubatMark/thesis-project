5da9308c3b2c9942b33c56424fb14275dfd1e457
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 12:10:45 -0800

    test

--- repository_similarity/urls.py
+++ repository_similarity/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
         path("admin/", admin.site.urls),
-        path("login/", include("Users.urls")),
+        path("", include("Users.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
