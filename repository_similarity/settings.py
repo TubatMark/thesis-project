2941550edec6266a8b68682502712924f0f59bb0
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 12:43:11 -0800

    cors

--- repository_similarity/settings.py
+++ repository_similarity/settings.py
@@ -56,6 +56,18 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 ROOT_URLCONF = "repository_similarity.urls"
 
