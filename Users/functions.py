364e4a46476d1134b901d836fae3f5a5d134cbf0
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 22:24:51 -0800

    corpora

--- Users/functions.py
+++ Users/functions.py
@@ -1,9 +1,7 @@
 import logging
 import PyPDF2
-import nltk
-nltk.download('stopwords','punkt')
 from nltk import pos_tag, word_tokenize
-from nltk.corpus import stopwords
+from nltk.corpora import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
