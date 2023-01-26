dc5794f706e624ce62e5b5acbc9a50466ed5be73
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-25 21:31:23 -0800

    punkt

--- Users/functions.py
+++ Users/functions.py
@@ -1,7 +1,7 @@
 import logging
 import PyPDF2
 import nltk
-nltk.download('stopwords')
+nltk.download('stopwords','punkt')
 from nltk import pos_tag, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
