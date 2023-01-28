aa8e4ba0ee458ea4c806f4874325b56c12a659e7
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-01-28 13:36:34 -0800

    settings in models

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission, AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
-
+from django.conf import settings
 
     
 class User(AbstractUser):
