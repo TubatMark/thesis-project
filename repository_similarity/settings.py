bc99865e2460a849b827709aa800378764fd205a
TubatMark <72871470+TubatMark@users.noreply.github.com>
2024-01-29 13:52:21 +0800

    .
    update edit and view cmi data.
    update user information and password
    update about

--- repository_similarity/settings.py
+++ repository_similarity/settings.py
@@ -56,10 +56,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
-CSRF_TRUSTED_ORIGINS = ['https://web-production-75a6.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://*.web-production-75a6.up.railway.app']
+# CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_HTTPONLY = True
+# CSRF_TRUSTED_ORIGINS = ['https://web-production-75a6.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://*.web-production-75a6.up.railway.app']
 
 WHITENOISE_ALLOW_ALL_ORIGINS = True
 
@@ -91,35 +91,36 @@ WSGI_APPLICATION = "repository_similarity.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "railway",
-        "USER": "root",
-        "PASSWORD": "MvuIw6n7iQeqQxOJbFJY",
-        "HOST": "containers-us-west-206.railway.app",
-        "PORT": "6028",
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
-        },
-    }
-}
-
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "thesis_db",
+#         "NAME": "railway",
 #         "USER": "root",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "3306",
+#         "PASSWORD": "MvuIw6n7iQeqQxOJbFJY",
+#         "HOST": "containers-us-west-206.railway.app",
+#         "PORT": "6028",
 #         "OPTIONS": {
 #             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
 #             "charset": "utf8mb4",
 #         },
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "thesis_db",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            # "charset": "utf8mb4",
+        },
+        'ENGINE': 'django.db.backends.mysql',
+    }
+}
 
 # DATABASES = {
 #     'default': {
